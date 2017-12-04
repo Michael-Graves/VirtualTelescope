f8c184fee601e7f71d2dbfb775b60de66925a13b
Michael-Graves <iamnewhb@gmail.com>
2017-12-03 22:07:18 -0800

    Adding GUI for coordinate driven navigation

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InputManager : MonoBehaviour {
 
@@ -13,6 +14,8 @@ public class InputManager : MonoBehaviour {
 
     public float[] RightAscension = { 0, 0, 0 };
     public float[] Declination = { 0, 0, 0 };
+
+    public Text field;
 
     // Use this for initialization
     void Start () {
@@ -27,14 +30,91 @@ public class InputManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(go) {
-            float rotY = -(RightAscension[0] + RightAscension[1] / 60.0f + RightAscension[2] / 3600.0f)*15;
-            float rotX = -(Mathf.Abs(Declination[0]) + Declination[1] / 60.0f + Declination[2] / 3600.0f) * Mathf.Sign(Declination[0]);
-            
-            horizon.rotation = Quaternion.Euler(new Vector3(Latitude, Longitude, 0));
-            horizon.transform.position = camera.transform.position - camera.transform.up * 0.1f;
-
-            camera.transform.rotation = Quaternion.Euler(new Vector3(rotX, rotY, 0));
+            Goto();
             go = false;
         }
-	}
+        string output = "none";
+        if(TouchScreenKeyboard.visible) {
+            output = "visible, ";
+            output += TouchScreenKeyboard.area.ToString();
+        } else {
+            output = "not visible, ";
+            output += TouchScreenKeyboard.area.ToString();
+        }
+        field.text = output;
+    }
+
+    protected void Goto() {
+        float rotY = -(RightAscension[0] + RightAscension[1] / 60.0f + RightAscension[2] / 3600.0f) * 15;
+        float rotX = -(Mathf.Abs(Declination[0]) + Declination[1] / 60.0f + Declination[2] / 3600.0f) * Mathf.Sign(Declination[0]);
+
+        horizon.rotation = Quaternion.Euler(new Vector3(Latitude, Longitude, 0));
+        horizon.transform.position = camera.transform.position - camera.transform.up * 0.1f;
+
+        camera.transform.rotation = Quaternion.Euler(new Vector3(rotX, rotY, 0));
+    }
+
+    public void Input_RA_H(InputField input) {
+        if(input.text != "") {
+            float result = 0;
+            if(float.TryParse(input.text, out result)) {
+                RightAscension[0] = Mathf.Clamp(result, 0, 24);
+                input.text = RightAscension[0].ToString();
+            }
+        }
+    }
+
+    public void Input_RA_M(InputField input) {
+        if (input.text != "") {
+            float result = 0;
+            if (float.TryParse(input.text, out result)) {
+                RightAscension[1] = Mathf.Clamp(result, 0, 60);
+                input.text = RightAscension[1].ToString();
+            }
+        }
+    }
+
+    public void Input_RA_S(InputField input) {
+        if (input.text != "") {
+            float result = 0;
+            if (float.TryParse(input.text, out result)) {
+                RightAscension[2] = Mathf.Clamp(result, 0, 60);
+                input.text = RightAscension[2].ToString();
+            }
+        }
+    }
+
+    public void Input_DEC_D(InputField input) {
+        if (input.text != "") {
+            float result = 0;
+            if (float.TryParse(input.text, out result)) {
+                Declination[0] = Mathf.Clamp(result, -90, 90);
+                input.text = Declination[0].ToString();
+            }
+        }
+    }
+
+    public void Input_DEC_M(InputField input) {
+        if (input.text != "") {
+            float result = 0;
+            if (float.TryParse(input.text, out result)) {
+                Declination[1] = Mathf.Clamp(result, 0, 60);
+                input.text = Declination[1].ToString();
+            }
+        }
+    }
+
+    public void Input_DEC_S(InputField input) {
+        if (input.text != "") {
+            float result = 0;
+            if (float.TryParse(input.text, out result)) {
+                Declination[2] = Mathf.Clamp(result, 0, 60);
+                input.text = Declination[2].ToString();
+            }
+        }
+    }
+
+    public void Input_GO() {
+        Goto();
+    }
 }
